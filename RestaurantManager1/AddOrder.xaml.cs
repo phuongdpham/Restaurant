4ad54df6e8c7f104b146c7cc47184a0c6e28ba32
Phuong D. Pham <phamduyphuong10@gmail.com>
2015-04-21 00:19:05 +0700

    no message

---
+++ RestaurantManager1/AddOrder.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RestaurantManager1
+{
+    /// <summary>
+    /// Interaction logic for AddOrder.xaml
+    /// </summary>
+    public partial class AddOrder : Page
+    {
+        public AddOrder()
+        {
+            InitializeComponent();
+            BindingTableNo();
+            BindingMenu();
+        }
+
+        private void BindingMenu()
+        {
+            using (var restaurantDB = new RestaurantContext())
+            {
+                var menus = restaurantDB.Dishs.Select(x => new { x.DishId, x.Name, x.Price, x.Status }).ToList();
+                gridMenu.ItemsSource = menus;
+                gridMenu.DataContext = menus;
+            }
+        }
+
+        private void BindingTableNo()
+        {
+            using (var restaurantDB = new RestaurantContext())
+            {
+                var dintableList = restaurantDB.DiningTables.Select(x => x.TableNo).ToList();
+                tableNoCB.ItemsSource = dintableList;
+                tableNoCB.SelectedItem = dintableList.FirstOrDefault();
+            }
+        }
+
+        private void addOrder_Cliked(object sender, RoutedEventArgs e)
+        {
+            string orderString = orderTB.Text.Trim();
+
+            if (String.IsNullOrEmpty(orderString))
+            {
+                MessageBox.Show("Order is empty. Please fill your orders in it.");
+                return;
+            }
+
+            string[] listOrder = Regex.Split(orderString, @"\W+");
+
+            int len = listOrder.Length;
+            int[] ids = new int[len / 2];
+            int[] quants = new int[len / 2];
+
+            // Chuyen cac so trong mang listOrder lan luot vao 2 mang ids va quants
+            for (int i = 0; i < len; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    ids[i / 2] = int.Parse(listOrder[i]);
+                }
+                else
+                {
+                    quants[i / 2] = int.Parse(listOrder[i]);
+                }
+            }
+
+            if (!checkValidData(ids))
+            {
+                MessageBox.Show("Order list have invalid number. Please check careful in menu!");
+                return;
+            }
+
+            // Add vao database
+            int tableNo = (int)tableNoCB.SelectedValue;
+            using (var rest = new RestaurantContext())
+            {
+                for (int i = 0; i < len/2; i++)
+                {
+                    rest.Orders.Add(new Order { TableNo = tableNo, DishId = ids[i], ReversedTime = DateTime.Now, Quantity = quants[i] });
+                }
+
+                rest.SaveChanges();
+            }
+
+            MessageBox.Show("Added orders to database!");
+            orderTB.Text = null;
+        }
+
+        private bool checkValidData(int[] ids)
+        {
+            using (var resource = new RestaurantContext())
+            {
+                var IdList = resource.Dishs.Select(x => x.DishId).ToList();
+
+                foreach (int item in ids)
+                {
+                    if (IdList.IndexOf(item) == -1)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+
+        private void finish_Clicked(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("FinishOrder.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        private void mainPage_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("MainPage.xaml", UriKind.RelativeOrAbsolute));
+        }
+    }
+}
---
+++ RestaurantManager1/FinishOrder.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RestaurantManager1
+{
+    /// <summary>
+    /// Interaction logic for FinishOrder.xaml
+    /// </summary>
+    public partial class FinishOrder : Page
+    {
+        public FinishOrder()
+        {
+            InitializeComponent();
+            BindingGridResult();
+            BindingComboBox();
+        }
+
+        private void BindingComboBox()
+        {
+            using (var res = new RestaurantContext())
+            {
+                var tableList = res.DiningTables.Select(x => x.TableNo).ToList();
+                tbCB.ItemsSource = tableList;
+                tbCB.SelectedItem = tableList.FirstOrDefault();
+            }
+        }
+
+        private void BindingGridResult()
+        {
+            using (var res = new RestaurantContext())
+            {
+                var listOrders = res.Orders.Join(res.Dishs, o => o.DishId, d => d.DishId,
+                                                 (o, d) => new { o.TableNo, o.DishId, d.Name, d.Price, o.Quantity }).ToList();
+
+                gridResult.ItemsSource = listOrders;
+            }
+        }
+
+        private void getReusult_Click(object sender, RoutedEventArgs e)
+        {
+            int tbNo = (int)tbCB.SelectedItem;
+            using (var res = new RestaurantContext())
+            {
+                var listOrders = res.Orders.Join(res.Dishs, o => o.DishId, d => d.DishId,
+                                                 (o, d) => new { o.TableNo, d.Price, o.Quantity }).
+                                                 Where(od => od.TableNo == tbNo).ToList();
+                int sum = 0;
+
+                foreach (var item in listOrders)
+                {
+                    sum += (int)item.Quantity * item.Price;
+                }
+
+                totalTBl.Text = sum.ToString();
+            }
+        }
+
+        private void clearOrders_Click(object sender, RoutedEventArgs e)
+        {
+            int tbNo = (int)tbCB.SelectedItem;
+            using (var res = new RestaurantContext())
+            {
+                var orders = res.Orders.Where(o => o.TableNo == tbNo).ToList();
+                foreach (var order in orders)
+                {
+                    res.Orders.Remove(order);
+                }
+                res.SaveChanges();
+                BindingGridResult();
+                totalTBl.Text = "0";
+            }
+        }
+    }
+}
